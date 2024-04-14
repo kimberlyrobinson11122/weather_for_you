185db5e4c035bc836c1a321f7eae58d494be337f
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-04-14 15:41:45 -0700

    got the current weather data from the search to work/fixed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # weather_for_you
+
+## 1. dynamically updated HTML and CSS.
+
+## 2. Use the 5 Day Weather ForecastLinks to an external site. to retrieve weather data for cities. The base URL should look like the following: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+
+# 3. You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather API
+
+GIVEN a weather dashboard with FORM INPUTS
+WHEN I search for a CITY
+THEN I am presented with CURRENT and FUTURE conditions for that city and that city is ADDED to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the CITY NAME, the DATE, an ICON representation of weather conditions, the TEMP, the HUMIDITY, and the the WIND SPEED
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
--- assets/style.js
+++ assets/style.js
@@ -1,2 +1,67 @@
+// use Openweathermap.org - use 5 day weather forecast
+// weather dashboard that will run in the browswer, dynamically HTML + CSS
+// API key (done)
+// Use localstorage to store any persistent data
+// current weather
+// 5 day weather
+// search + adds your searches to the search history button (up to ?)
 
+var searchInputEl = document.querySelector('.searchInput');
 
+//var x = document.getElementById("myText").value;
+
+const apiKey = `1fdef56e946e4c8e8868a0c1341b24c4`;
+var cityName = `New York`
+
+let searchBtn = document.querySelector('.searchButton');
+searchBtn.addEventListener("click", function() {
+    cityName = searchInputEl.value;
+    const geoApi = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`
+    const 
+
+    fetch(geoApi)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data){
+            //lat
+            //lon
+            console.log(data);
+        })
+});
+
+//element.addEventListener("click", function(){ alert("Hello World!"); });
+
+// openweather api url
+// const weatherData = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+
+// console.log(weatherData);
+
+// fetch(weatherData)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+// });
+
+// console.log(weatherData);
+
+//api.openweathermap.org/data/2.5/weather?q={city}&appid={APIkey};
+
+// const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+// fetch(queryURL)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     });
+
+// console.log(queryURL);
+
+// coord: {lon: xxx.xxx lat: xxx.xxx}
+// main: {temp: 294.14, feels like: xxx, temp_min: xxx, temp_max: xxx.xx, pressure: xxxx., name: New York}
+// weather: {description: "clear sky". icon: "01d", id: xxx, main: }
+// wind: {deg: xxx, gust: xx.xx, speed: xx.xx}
